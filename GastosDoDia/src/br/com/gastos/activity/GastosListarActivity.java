966df954bfd1cbae8b569567ffb9d4dcb6e9cd92
leonardoMetalhead <leonardoMetalhead@792768a2-251c-12ab-d5fa-d1e2a4d04564>
2010-12-07 23:22:47 +0000

    Implementação da seleção dos custos cadastrados.

--- GastosDoDia/src/br/com/gastos/activity/GastosListarActivity.java
+++ GastosDoDia/src/br/com/gastos/activity/GastosListarActivity.java
@@ -3,77 +3,94 @@ package br.com.gastos.activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.CursorAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 import br.com.gastos.adapter.DBAdapter;
 
-public class GastosListarActivity extends Activity {
+public class GastosListarActivity extends ListActivity {
 
 	private Context context;
 	private DBAdapter dbAdapter;
+	private ListView listAllGastos;
+	
+	public static final String ALTERAR = "Alterar";
+	public static final String EXCLUIR = "Excluir";
+	public static final String DESCRICAO = "Descrição";
+	public static final String GASTOS = "Gastos...";
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
-		
-	   	context = this;
-    	dbAdapter = new DBAdapter(context);
-    	
-    	setContentView(R.layout.list_todos_gastos);
-    	
-    	
-    	dbAdapter.open();
-    		Cursor allGastos = dbAdapter.getAllGastos();
-    		List<String> listGastos = new ArrayList<String>();
-    		
-    		if(allGastos.moveToFirst()) {
-    			do {
-					listGastos.add(allGastos.getString(1));
-				} while (allGastos.moveToNext());
-    		}
-    	dbAdapter.close();
+
+		context = this;
+
+		dbAdapter = new DBAdapter(context);
+
+		setContentView(R.layout.list_todos_gastos);
+
+		dbAdapter.open();
+
+		Cursor allGastos = dbAdapter.getAllGastos();
 	
-    	ListView listAllGastos = (ListView) findViewById(R.id.listTodosGastos);
-    	listAllGastos.setSoundEffectsEnabled(true);
-    	listAllGastos.setAdapter(new ArrayAdapter<String>(context, R.layout.list_item, listGastos));
-//    	listAllGastos.setAdapter(new CursorAdapter(context, allGastos) {
-//			
-//			@Override
-//			public View newView(Context context, Cursor cursor, ViewGroup parent) {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//			
-//			@Override
-//			public void bindView(View view, Context context, Cursor cursor) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		});
-    	
-    	listAllGastos.setOnItemSelectedListener(new OnItemSelectedListener() {
+		List<String> listGastos = new ArrayList<String>();
 
-			public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				
-			}
+		if (allGastos.moveToFirst()) {
+			
+			do {
 
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
+				listGastos.add(allGastos.getString(1));
+
+			} while (allGastos.moveToNext());
+		}
+
+		dbAdapter.close();
+
+		listAllGastos = getListView();
+		listAllGastos.setSoundEffectsEnabled(true);
+
+		// TODO:Verificar por que não consigo usar o layout list_item.xml.
+		// Explicando: ao clicar na linha com o item, o metodo do listener
+		// nao é invocado, pesquisando rapido na net vi um exemplo onde se usava
+		// esse layout padrao do android.
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listGastos);
+
+		listAllGastos.setAdapter(adapter);
+
+		listAllGastos.setTextFilterEnabled(true);
+
+		listAllGastos.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> a, View v, int position,	long id) {
 				
+				final CharSequence[] items = { ALTERAR, EXCLUIR, DESCRICAO };
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(GastosListarActivity.this);
+
+				builder.setTitle(GASTOS);
+				
+				builder.setItems(items, new DialogInterface.OnClickListener() {
+				
+					public void onClick(DialogInterface dialog, int item) {
+						Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();							
+					}
+				});
+
+				AlertDialog alert = builder.create();
+				alert.show();
 			}
 		});
 	}
-	
 }
