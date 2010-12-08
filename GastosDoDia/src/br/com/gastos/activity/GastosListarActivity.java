ed039a41186e6589f8176426b250b67207ba7f95
ythalorossy <ythalorossy@792768a2-251c-12ab-d5fa-d1e2a4d04564>
2010-12-08 04:27:07 +0000

    - Adicionada imagem de background na tela principal. - Novo Adapter para listar mais informações na listagem completa de gastos. - Modificado os layout (xml) de listagem de gastos

--- GastosDoDia/src/br/com/gastos/activity/GastosListarActivity.java
+++ GastosDoDia/src/br/com/gastos/activity/GastosListarActivity.java
@@ -1,31 +1,30 @@
 package br.com.gastos.activity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 import br.com.gastos.adapter.DBAdapter;
 
+/**
+ * Listagem de todos os gastos efetuados;
+ * @author yross
+ *
+ */
 public class GastosListarActivity extends DefaultGastosListActtivity {
 
 	private DBAdapter dbAdapter;
-	private ListView listAllGastos;
 	
 	public static final String ALTERAR = "Alterar";
 	public static final String EXCLUIR = "Excluir";
 	public static final String DESCRICAO = "Descrição";
 	public static final String GASTOS = "Gastos...";
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -35,37 +34,26 @@ public class GastosListarActivity extends DefaultGastosListActtivity {
 
 		setContentView(R.layout.list_todos_gastos);
 
-		dbAdapter.open();
-
-		Cursor allGastos = dbAdapter.getAllGastos();
-	
-		List<String> listGastos = new ArrayList<String>();
-
-		if (allGastos.moveToFirst()) {
-			
-			do {
-
-				listGastos.add(allGastos.getString(1));
-
-			} while (allGastos.moveToNext());
-		}
-
-		dbAdapter.close();
-
-		listAllGastos = getListView();
-		listAllGastos.setSoundEffectsEnabled(true);
+		Cursor cursor = dbAdapter.open().getAllGastos();
 
 		// TODO:Verificar por que não consigo usar o layout list_item.xml.
 		// Explicando: ao clicar na linha com o item, o metodo do listener
 		// nao é invocado, pesquisando rapido na net vi um exemplo onde se usava
 		// esse layout padrao do android.
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listGastos);
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listGastos);
+		
+		// Conseguir passar mais informações para o List, desta vez estou passando um Cursor no ligar da Lsta de Strings
+		// Dica: http://thinkandroid.wordpress.com/2010/01/09/simplecursoradapters-and-listviews/
+		
+		startManagingCursor(cursor);
+        String[] columns = new String[] { DBAdapter.KEY_DESCRICAO, DBAdapter.KEY_VALOR, DBAdapter.KEY_DATA};
+        int[] to = new int[] { R.id.item_list_gasto_descricao, R.id.item_list_gasto_valor, R.id.item_list_gasto_data};
+        
+		SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.list_item_gasto, cursor, columns, to);
+		
+		setListAdapter(cursorAdapter);
 
-		listAllGastos.setAdapter(adapter);
-
-		listAllGastos.setTextFilterEnabled(true);
-
-		listAllGastos.setOnItemClickListener(new OnItemClickListener() {
+		getListView().setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> a, View v, int position,	long id) {
