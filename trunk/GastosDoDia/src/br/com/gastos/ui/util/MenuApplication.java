78cb0b5c17bceede307fde16c94c9cd975fe43a6
leonardoMetalhead <leonardoMetalhead@792768a2-251c-12ab-d5fa-d1e2a4d04564>
2010-12-08 02:03:34 +0000

    Criação de interface que implementa a função do menu.

---
+++ trunk/GastosDoDia/src/br/com/gastos/ui/util/MenuApplication.java
@@ -0,0 +1,14 @@
+package br.com.gastos.ui.util;
+
+import android.view.View.OnCreateContextMenuListener;
+
+/**
+ * Interface criada para abstrair a logica da criação do menu para todas as telas
+ * da aplicação.
+ * 
+ * @author leonardo - leonardometalhead@gmail.com
+ *
+ */
+public interface MenuApplication extends OnCreateContextMenuListener {
+
+}
