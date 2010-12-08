dbbe9a3c48fffe90215570aa7a3a8158842ead7f
leonardoMetalhead <leonardoMetalhead@792768a2-251c-12ab-d5fa-d1e2a4d04564>
2010-12-08 02:03:01 +0000

    Criação de classes que fazem uma abstração com as classes Activity e ListActivity.

--- trunk/GastosDoDia/src/br/com/gastos/activity/GastosActivity.java
+++ trunk/GastosDoDia/src/br/com/gastos/activity/GastosActivity.java
@@ -1,52 +1,14 @@
 package br.com.gastos.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 
-public class GastosActivity extends Activity {
-	
-	private Context context;
+public class GastosActivity extends DefaultGastosActivity {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	
-    	context = this;
-    	
     	super.onCreate(savedInstanceState);
         
     	setContentView(R.layout.main);
     }
     
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	MenuInflater menuInflater = getMenuInflater();
-    	menuInflater.inflate(R.menu.menu, menu);
-
-    	MenuItem menuItemNovoGasto = menu.findItem(R.id.item01);
-    	menuItemNovoGasto.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			public boolean onMenuItemClick(MenuItem item) {
-				Intent myIntent = new Intent(context, GastosNovoActivity.class);
-                startActivityForResult(myIntent, 0);
-				return false;
-			}
-		});
-    	
-    	MenuItem menuItemListarGasto = menu.findItem(R.id.item02);
-    	menuItemListarGasto.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			public boolean onMenuItemClick(MenuItem item) {
-				Intent myIntent = new Intent(context, GastosListarActivity.class);
-                startActivityForResult(myIntent, 0);
-				return false;
-			}
-		});
-    	
-    	// TODO Auto-generated method stub
-    	return super.onCreateOptionsMenu(menu);
-    }
 }
