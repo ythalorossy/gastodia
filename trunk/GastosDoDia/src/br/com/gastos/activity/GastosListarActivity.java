e81fdd9ef9412afac08a0239efbe58ae4475e9eb
ythalorossy <ythalorossy@792768a2-251c-12ab-d5fa-d1e2a4d04564>
2010-12-09 03:19:48 +0000

    Na listagem de Gastos:
    - Ao clicar em um gasto a descricão aparecer em um popup.
    - Al manter o gasto precionado aparecer as opções Alterar e Excluir.

--- trunk/GastosDoDia/src/br/com/gastos/activity/GastosListarActivity.java
+++ trunk/GastosDoDia/src/br/com/gastos/activity/GastosListarActivity.java
@@ -2,17 +2,17 @@ package br.com.gastos.activity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+import br.com.gastos.activity.GastosListarActivity.Opcoes;
 import br.com.gastos.adapter.DBAdapter;
-import br.com.gastos.ui.util.Constants;
 
 /**
  * Listagem de todos os gastos efetuados;
@@ -23,11 +23,17 @@ public class GastosListarActivity extends DefaultGastosListActtivity {
 
 	private DBAdapter dbAdapter;
 	
+	enum Opcoes {
+		ALTERAR("Alterar"), EXCLUIR("Excluir");
+		private String descricao;
+		private Opcoes(String descricao) {this.descricao = descricao;}
+		public String toString() {return this.descricao;}
+	};
+	
 	public static final String ALTERAR = "Alterar";
 	public static final String EXCLUIR = "Excluir";
 	public static final String DESCRICAO = "Descrição";
 	public static final String GASTOS = "Gastos...";
-
 	
 	private ListView listView = null;
 	@Override
@@ -52,18 +58,21 @@ public class GastosListarActivity extends DefaultGastosListActtivity {
 		String[] columns = new String[] { DBAdapter.KEY_DESCRICAO};
 		int[] to = new int[] { R.id.item_list_gasto_descricao};
 		
-		SimpleCursorAdapter cursorAdapter =
-			new SimpleCursorAdapter(this, R.layout.list_item_gasto, cursor, columns, to);
+		SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(
+													this, 
+													R.layout.list_item_gasto, 
+													cursor, 
+													columns, 
+													to);
 		
 		setListAdapter(cursorAdapter);
 
 		listView = getListView();
-		listView.setOnItemClickListener(new OnItemClickListener() {
+		listView.setOnItemLongClickListener(new OnItemLongClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> a, View v, int position,	long id) {
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 				
-				final CharSequence[] items = { ALTERAR, EXCLUIR, DESCRICAO };
+				final CharSequence[] items = { Opcoes.ALTERAR.toString(), Opcoes.EXCLUIR.toString()};
 				
 				AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
@@ -73,28 +82,83 @@ public class GastosListarActivity extends DefaultGastosListActtivity {
 				
 					public void onClick(DialogInterface dialog, int item) {
 						
-						switch (item) {
-						case 0:
-							// TODO: implementar logica necessaria
+						Opcoes opc = Opcoes.values()[item];
+						
+						switch (opc) {
+						case ALTERAR:
+						
+							Toast.makeText(context, "Alterar", Toast.LENGTH_SHORT);
 							break;
-						case 1:
-							// TODO: implementar logica necessaria
-							break;
-						case 2:
 
-							Intent myIntent = new Intent(context, GastosDescricaoActivity.class);
+						case EXCLUIR:
 							
-							TextView txt = (TextView)findViewById(R.id.item_list_gasto_descricao);
-							myIntent.putExtra(Constants.CAMPO_DESCRICAO, txt.getText());
-							
-							startActivity(myIntent);
-							
+							Toast.makeText(context, "Alterar", Toast.LENGTH_SHORT);
 							break;
+						
+//						case 2:
+//
+//							Intent myIntent = new Intent(context, GastosDescricaoActivity.class);
+//							
+//							TextView txt = (TextView)findViewById(R.id.item_list_gasto_descricao);
+//							myIntent.putExtra(Constants.CAMPO_DESCRICAO, txt.getText());
+//							
+//							startActivity(myIntent);
+//							
+//							break;
 						default:
 							break;
 						}
 					}
 				});
+
+				builder.create().show();
+				
+				return false;
+			}
+		});
+		
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> a, View v, int position,	long id) {
+				
+				SimpleCursorAdapter adapter = (SimpleCursorAdapter) a.getAdapter();
+				
+				String descricao = adapter.getCursor().getString(1);
+				float valor = adapter.getCursor().getFloat(2);
+//				
+				AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+				builder.setTitle(GASTOS);
+				builder.setMessage(String.format("Descricao: %s \n Valor: R$%.2f", descricao, valor));
+				
+//				builder.setItems(items, new DialogInterface.OnClickListener() {
+//				
+//					public void onClick(DialogInterface dialog, int item) {
+//						
+//						switch (item) {
+//						case 0:
+//							// TODO: implementar logica necessaria
+//							break;
+//						case 1:
+//							// TODO: implementar logica necessaria
+//							break;
+//						case 2:
+//
+//							Intent myIntent = new Intent(context, GastosDescricaoActivity.class);
+//							
+//							TextView txt = (TextView)findViewById(R.id.item_list_gasto_descricao);
+//							myIntent.putExtra(Constants.CAMPO_DESCRICAO, txt.getText());
+//							
+//							startActivity(myIntent);
+//							
+//							break;
+//						default:
+//							break;
+//						}
+//					}
+//				});
 
 				AlertDialog alert = builder.create();
 				alert.show();
